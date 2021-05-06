8c7742d1f37182546561730cc29433f80ae324a0
rohan-joby <rohanjobyak@gmail.com>
2021-05-06 20:52:40 +0530

    wip: logic to display the matched city and state names

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const cities=[]; //to store city details
-const url="cities.json"; //url of json containing data
+let cities = []; //to store city details
+const url = "cities.json"; //url of json containing data
 
 fetch(url)
-    .then(data => data.json())
-    .then(value => cities=[...value]); //to store city details from json in "cities" array
+  .then((data) => data.json())
+  .then((value) => (cities = [...value])); //to store city details from json in "cities" array
 
 const input = document.querySelector(".input-field"); //to access user input
 const result = document.querySelector(".suggestion"); //to access suggestions dropdown
 
-input.addEventListener("keyup",showResults); //detect user input
-input.addEventListener("change",showResults); //detect user input
+input.addEventListener("keyup", showResults); //detect user input
+input.addEventListener("change", showResults); //detect user input
 
-function findResults(stringToCheck,cities){
-    return cities.filter(place =>{
-        const regex1 = new RegExp(stringToCheck,'gi');
-        return place.name.match(regex1) || place.state.match(regex1);
-    })
+function findResults(stringToCheck, cities) {
+  //to find name sof matching places
+  return cities.filter((place) => {
+    const regex1 = new RegExp(stringToCheck, "gi");
+    return place.name.match(regex1) || place.state.match(regex1);
+  });
 }
-function showResults(){}
 
+function showResults() {
+  //to display result of search
+  const placesFound = findResults(this.value, cities);
+  const html = placesFound.map((place) => {
+    return `<ul class="suggestion"><li class="suggestion-item">${place.name}, ${place.state}</li></ul>`;
+  });
+  const stringText = html.join(" ");
+  result.innerHTML = stringText;
+}
